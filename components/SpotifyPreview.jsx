ed6a45ee683d6ac33434a008d8d5af44f1049ec0
Fetters83 <wgyves@hotmail.com>
2025-02-07 15:27:46 +0000

    removed the keyword new on promise.rejects on api.js

--- components/SpotifyPreview.jsx
+++ components/SpotifyPreview.jsx
@@ -7,6 +7,8 @@ export default function SpotifyPreview ({ spotifyTrack }) {
     const myHtml = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${spotifyTrack.id}" width="90%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
     const webViewRef = useRef(null);
 
+    
+
     return (
         <View style={{}}>
             <View style={{width: "100%", height: 352, marginLeft: "31%", backgroundColor: 'transparent'}}>
